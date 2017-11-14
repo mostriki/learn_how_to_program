297614fd99ce3aa098a15c5bb872dbae10d5796b
David Hale and Riki Montgomery <riki@>
2017-11-14 15:50:58 -0800

    add CRUD functionality

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -4,4 +4,43 @@ class LessonsController < ApplicationController
     @section = Section.find(@lesson.section_id)
     @course = Course.find(@section.course_id)
   end
+
+  def new
+    @lesson = Lesson.new
+    @section = Section.all
+  end
+
+  def create
+    @lesson = Lesson.new(lesson_params)
+    if @lesson.save
+      redirect_to  courses_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @lesson = Lesson.find(params[:id])
+    @section = Section.all
+  end
+
+  def update
+    @lesson= Lesson.find(params[:id])
+    if @lesson.update(lesson_params)
+      redirect_to section_path(@lesson.section_id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @lesson = Lesson.find(params[:id])
+    @lesson.destroy
+    redirect_to section_path(@lesson.section_id)
+  end
+
+  private
+  def lesson_params
+    params.require(:lesson).permit(:title, :content, :section_id, :day, :lesson_type)
+  end
 end
--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -3,4 +3,43 @@ class SectionsController < ApplicationController
     @section = Section.find(params[:id])
     @course = Course.find(@section.course_id)
   end
+
+  def new
+    @section = Section.new
+    @course = Course.all
+  end
+
+  def create
+    @section = Section.new(section_params)
+    if @section.save
+      redirect_to  courses_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @section = Section.find(params[:id])
+    @course = Course.all
+  end
+
+  def update
+    @section= Section.find(params[:id])
+    if @section.update(section_params)
+      redirect_to course_path(@section.course_id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @section = Section.find(params[:id])
+    @section.destroy
+    redirect_to courses_path
+  end
+
+  private
+  def section_params
+    params.require(:section).permit(:title, :week, :course_id)
+  end
 end
